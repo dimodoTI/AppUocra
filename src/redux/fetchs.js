4024f1f20e0af42b361324cf4d7d97780f988b54
Manuel Dominguez <mdominguez@uocra.org>
2020-08-03 15:44:41 -0300

    cambio Fetch.js con Ip de uocra.net

--- src/redux/fetchs.js
+++ src/redux/fetchs.js
@@ -7,8 +7,12 @@ import {
   fetchFactory
 } from "../libs/fetchFactory"
 
-const webApiMenuOdata = "http://200.80.227.180/appuocra"
-const webApiNoticiaOdata = "http://200.80.227.180/appuocra"
+/* const webApiMenuOdata = "http://200.80.227.180/appuocra"
+const webApiNoticiaOdata = "http://200.80.227.180/appuocra" */
+
+
+const webApiMenuOdata = "https://www.uocra.net/AppUOCRA"
+const webApiNoticiaOdata = "https://www.uocra.net/AppUOCRA"
 
 const menuOdata = ODataFetchFactory({
   fetch: fetch,
